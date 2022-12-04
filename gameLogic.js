ca8ddcb8568fcabfab53c4d99d981409bd440b03
Tyler Best <tylerbest@Tylers-Air.modem>
2022-12-05 09:50:39 +1100

    if player is computer, then the computer plays a random turn. Game in pretty good state right now.

--- gameLogic.js
+++ gameLogic.js
@@ -2,8 +2,10 @@
 // Establishing the game board as an object 
 
 const gameBoard = (() => {
-    let board = new Array(9);
+    let board = Array.apply(null, Array(9));
     let player1Turn = true;
+    let isGameTied = false;
+    let firstClick = true;
 
     let gameOver = false;
 
@@ -24,6 +26,17 @@ const gameBoard = (() => {
             console.log(board)
         }
     }
+
+    const emptyBoardPositions = () => {
+        const emptyPositions = board.reduce((acc, curr, index) => {
+            if (curr == null){
+                acc.push(index);
+            }
+            return acc;
+        }
+        , []);
+        return emptyPositions;
+    }
     
     const hasMoveBeenDone = (boardPosition) => {
         if (board[boardPosition] == "p1" || board[boardPosition] == "p2") {
@@ -32,6 +45,18 @@ const gameBoard = (() => {
     }
 
     const gameOverTest = () => {
+        // want to quickly test if the gameboard is completely filled for a tie first
+        let counter = 0
+        board.forEach((index) => {
+            if (index == null) {
+                isGameTied = false
+            } else {counter++}
+        });
+        if (counter == 9) {
+            isGameTied = true;
+            return ["tie", true]
+        }
+
         // all horizontal wins
         if (board[0] == "p1" && board[1] == "p1" && board[2] == "p1") {
             return ["p1", true]
@@ -86,7 +111,7 @@ const gameBoard = (() => {
         else return ["NA",false]
     }
 
-    return {board, changeTurn, newMove, getPlayerTurn, hasMoveBeenDone, gameOverTest, gameOver};
+    return {board, changeTurn, newMove, getPlayerTurn, hasMoveBeenDone, gameOverTest, emptyBoardPositions, gameOver, firstClick};
 })();
 
 const Player = (name) => {
@@ -111,7 +136,19 @@ console.log(vsString);
 var blocks = document.getElementsByClassName('gameBlocks');
 let i = 0;
 
+function gameOver(overBool, message) {
+    gameBoard.gameOver = overBool;
+    // removing the hover class from the blocks
+    var blocks = document.getElementsByClassName('gameBlocks');
+    for (let block of blocks) {
+        block.classList.remove('hoverClass');
+    }
+    let winnertext = document.getElementById('winnerText');
+    winnertext.innerHTML = message;
+}
+
 const blockClicked = e => {
+
     if (gameBoard.gameOver == false) {
         console.log(e.target.id);
         idString = e.target.id; 
@@ -137,20 +174,41 @@ const blockClicked = e => {
             // Tell the game that it is now the other players turn
             gameBoard.changeTurn();
         }
+
+        const whoseTurn = document.getElementById('instructions');
+        let player1Turn = gameBoard.getPlayerTurn()
+        if (player1Turn) {
+            whoseTurn.innerHTML = `${player1.name}'s turn`
+        } else {whoseTurn.innerHTML = `${player2.name}'s turn`}
     
         // Run a check on the gameboard to see if anyone has won yet
         [winner,overBool] = gameBoard.gameOverTest()
+        
+        if (winner == "tie") {
+            console.log("The test was successful and the game should be tied");
+            gameOver(overBool, "Sucked in, nobody wins in a tie!");
+        } 
+        // If a player has won the game
         if (overBool) {
             console.log("The test was successful and the game should be over");
-            gameBoard.gameOver = overBool;
-            // removing the hover class from the blocks
-            var blocks = document.getElementsByClassName('gameBlocks');
-            for (let block of blocks) {
-                block.classList.remove('hoverClass');
+            if (winner == "p1") {
+                gameOver(overBool, `Congratulations to ${player1.name}, you have won the game!`);
             }
-            let winnertext = document.getElementById('winnerText');
-
-            winnertext.innerHTML = `Congratulations to ${winner} for winning the match!`
+            else {
+                gameOver(overBool, `Congratulations to ${player2.name}, you have won the game!`);
+            }
+        }
+        // whoseTurn = gameBoard.getPlayerTurn;
+        if ((player2.name == "Computer") && (gameBoard.getPlayerTurn() == false)) {
+            // Since player 2 is the computer we want to select a random empty cell for the computers turn
+            emptyIndexes = gameBoard.emptyBoardPositions();
+            // We now have the list of empty indexes. Now to select a random value from that list
+            const randomMove = emptyIndexes[Math.floor(Math.random() * emptyIndexes.length)];
+            gameBoard.newMove(randomMove)
+            // Need to update the board now
+            let idString = 'block-' + (randomMove + 1);
+            document.getElementById(idString).style.backgroundColor = "red";
+            gameBoard.changeTurn();
         }
     }
     
@@ -160,10 +218,22 @@ for (let block of blocks) {
     block.addEventListener("click", blockClicked);
 }
 
+let instructions = document.getElementById('instructions');
+let blueColour = document.getElementById('blueColour');
+let redColour = document.getElementById('redColour');
 
 const resultsList = document.getElementById('results')
 new URLSearchParams(window.location.search).forEach((value,
     name) => {
-        resultsList.append(`${name}: ${value}`)
-        resultsList.append(document.createElement('br'))
+        if (name == "player1") {
+            player1.name = value;
+        } else if (name == "player2") {
+            player2.name = value;
+        }
+        let vsString = `${player1.name} vs ${player2.name}! GAME ON!`;
+        vsInfo.innerHTML = vsString;
+
+        instructions.innerHTML = `${player1.name} goes first`
+        blueColour.innerHTML = `Blue = ${player1.name}`
+        redColour.innerHTML = `Red = ${player2.name}`
     })
